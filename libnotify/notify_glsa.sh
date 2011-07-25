20a9736b987b1cec130f9244d59ce304938f50ef
Mathias Kub <git@makubi.at>
2011-07-25 20:17:41 +0200

    GLSA-Check wird mit Nice-Level 19 ausgefuehrt.

--- libnotify/notify_glsa.sh
+++ libnotify/notify_glsa.sh
@@ -11,20 +11,26 @@
 #	/usr/bin/glsa-check -t all
 #
 # -- e.g.
-#	Cmnd_Alias	SYNC_PORTAGE = /usr/bin/emerge --sync --quiet
-#	Cmnd_Alias	CHECK_GLAS = /usr/bin/glsa-check -t all
+#	Cmnd_Alias	SYNC_PORTAGE = /usr/bin/nice -n $NICE_LVL /usr/bin/emerge --sync --quiet
+#	Cmnd_Alias	CHECK_GLAS = /usr/bin/nice -n $NICE_LVL /usr/bin/glsa-check -t all
 #	
 #	myuser ALL=NOPASSWD: SYNC_PORTAGE_TREE
 #	myuser ALL=NOPASSWD: CHECK_GLSAS
 
-# sync portage tree
-sudo emerge --sync --quiet
+NICE_BIN=/usr/bin/nice
+EMERGE_BIN=/usr/bin/emerge
+NOTIFY_BIN=/usr/bin/notify-send
+GLSA_BIN=/usr/bin/glsa-check
 
-NOTIFY_BIN="/usr/bin/notify-send"
+NICE_LVL=19
 CHECK_OK_STRING="This system is not affected by any of the listed GLSAs"
 
+# sync portage tree
+sudo $NICE_BIN -n $NICE_LVL $EMERGE_BIN --sync --quiet
+
+
 # check all GLSAs
-GLSA_OUTPUT=`sudo glsa-check -t all 2>&1`
+GLSA_OUTPUT=`sudo $NICE_BIN -n $NICE_LVL $GLSA_BIN -t all 2>&1`
 
 # inform user
 if [ "$GLSA_OUTPUT" == "$CHECK_OK_STRING" ]
