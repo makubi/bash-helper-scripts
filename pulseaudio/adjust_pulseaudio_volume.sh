f8a5673fd2b40e7de1915ad6e93fcf2dcccdfe8e
Mathias Kub <git@makubi.at>
2011-11-19 18:58:24 +0100

    Ueberschreite Lautstaerke von 100% nicht.

--- pulseaudio/adjust_pulseaudio_volume.sh
+++ pulseaudio/adjust_pulseaudio_volume.sh
@@ -2,6 +2,8 @@
 #
 # Adjusts the volume using 'pacmd' and 'pactl' (Pulseaudio).
 
+MAX_VOLUME="0x10000"
+
 if [ $# -eq 1 ]
 then
 	VOLUME_RELATIVE=$1
@@ -10,15 +12,23 @@ then
 	SINK_VOLUME_OUTPUT=`pacmd dump | grep set-sink-volume`
 	
 	TMPFILE=`mktemp`
+	
 	pacmd dump | grep set-sink-volume > $TMPFILE
 	
 	while read SINK_VOLUME_LINE
 	do
 		SINK_NAME=`echo $SINK_VOLUME_LINE | awk '{ print $2 }'`
 		SINK_VOLUME=`echo $SINK_VOLUME_LINE | awk '{ print $3 }'`
-		
-		# sets new volume
-		pactl set-sink-volume "$SINK_NAME" $(( $SINK_VOLUME + $VOLUME_RELATIVE ))
+		NEW_VOLUME_SUM=$(( $SINK_VOLUME + $VOLUME_RELATIVE ))
+		MAX_VOLUME_RELATIVE=$(( $MAX_VOLUME - $NEW_VOLUME_SUM ))
+
+		 # sets new volume
+		if [ $MAX_VOLUME_RELATIVE -ge 0 ]
+		then
+			pactl set-sink-volume "$SINK_NAME" $NEW_VOLUME_SUM
+		else
+			pactl set-sink-volume "$SINK_NAME" $MAX_VOLUME
+		fi
 	done < $TMPFILE
 	
 	rm $TMPFILE
